2e40cacf2ad368640e47d5bdcd0d0ad7f722b391
Lambda_School_Loaner_96 <lambda_school_loaner_96@LambdaSer96sMBP.attlocal.net>
2019-05-13 18:33:02 -0400

    commit

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <div className='heart'>
+            <i class="fas fa-comments"></i>
+
+
+            <i class="far fa-heart">4</i>
+
+            <i class="far fa-envelope-open"></i>
+
+
+
+
+        </div>
+
+
+
+       
+
+
+    )
+   
+}
+export default Footer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+function HeaderTitle (){
+    return (
+        <h2 className='head-title'>Lambda School <span className='atLambda'>@LambdaSchool</span> <span className='dot'>•</span><span className="date">07 jan</span></h2>
+    )
+}
+export default HeaderTitle
